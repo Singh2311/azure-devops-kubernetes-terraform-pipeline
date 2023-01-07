aa937341fefd29f1e02d72a9041c5c7f4563f923
Singh2311 <ss9532729@gmail.com>
2023-01-07 22:08:34 +0530

    change subnet ids

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
@@ -71,5 +71,5 @@ resource "kubernetes_cluster_role_binding" "example" {
 
 # Needed to set the default region
 provider "aws" {
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
