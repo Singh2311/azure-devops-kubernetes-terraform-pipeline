ed253e4d3c798d1dbe765b0c001f880781d8d324
Singh2311 <ss9532729@gmail.com>
2023-01-07 20:37:16 +0530

    change subnet ids

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -25,13 +25,12 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
 }
 
 module "sumit-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "sumit-cluster"
-  cluster_version = "1.14"
+  cluster_version = "1.17"
   subnets         = ["subnet-072b3355fe2ae6bc4", "subnet-06dde89b4281a08ff"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
