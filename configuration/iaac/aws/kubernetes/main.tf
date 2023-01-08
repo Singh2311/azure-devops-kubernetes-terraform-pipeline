f7a5fcb39c1e14323b8dc4205eac568f33ffeeaa
Singh2311 <ss9532729@gmail.com>
2023-01-08 14:39:18 +0530

    fix issue

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -25,7 +25,7 @@ data "aws_subnets" "default_subnets" {
 }
 
 provider "kubernetes" {
-  host                   = dmodule.eks.cluster_endpoint
+  host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
