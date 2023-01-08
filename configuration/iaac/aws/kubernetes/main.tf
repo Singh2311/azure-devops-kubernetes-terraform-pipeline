25ab63bb5610f650f6a480ed033da4af6027ef34
Singh2311 <ss9532729@gmail.com>
2023-01-08 13:03:40 +0530

    fix issue

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -59,10 +59,20 @@ resource "kubernetes_cluster_role_binding" "example" {
     kind      = "ClusterRole"
     name      = "cluster-admin"
   }
+ subject {
+    kind      = "User"
+    name      = "admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
   subject {
     kind      = "ServiceAccount"
     name      = "default"
-    namespace = "default"
+    namespace = "kube-system"
+  }
+  subject {
+    kind      = "Group"
+    name      = "system:masters"
+    api_group = "rbac.authorization.k8s.io"
   }
 }
 
